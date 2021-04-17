f095dae5e42c5ff48087fe5436e28c1a0d3b5529
JarodBuus <81185388+JarodBuus@users.noreply.github.com>
2021-04-16 17:59:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+
+//website button logic
+//#region       
+function element() {
+    let newEgg = document.getElementById("newEgg");
+    let sw = document.getElementById("sw");
+    let xt = document.getElementById("xt");
+    let ars = document.getElementById("ars");
+    let liSw = document.getElementById("li-sw");
+}
+function changeSizeLNewEgg(x) {
+    x.style.height = "110px";
+    x.style.width = "110px";
+    console.log("Bigger!");
+    newEgg.style.height = "110px";
+    newEgg.style.width = "110px";
+    element();
+}
+function changeSizeSNewEgg(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+    console.log("Normal.");
+    newEgg.style.height = "100px";
+    newEgg.style.width = "100px";
+    element();
+}
+function changeSizeLSw(x) {
+    x.style.height = "110px";
+    x.style.width = "110px";
+    console.log("Normal.");
+    sw.style.height = "110px";
+    sw.style.width = "110px";
+    element();
+}
+function changeSizeSSw(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+    console.log("Bigger!");
+    sw.style.height = "100px";
+    sw.style.width = "100px";
+    element();
+}
+function changeSizeLXt(x) {
+    x.style.height = "110px";
+    x.style.width = "110px";
+    console.log("Normal.");
+    xt.style.height = "110px";
+    xt.style.width = "110px";
+    element();
+}
+function changeSizeSXt(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+    console.log("Normal.");
+    xt.style.height = "100px";
+    xt.style.width = "100px";
+    element();
+}
+function changeSizeLArs(x) {
+    x.style.height = "110px";
+    x.style.width = "110px";
+    console.log("Bigger!");
+    ars.style.height = "110px";
+    ars.style.width = "110px";
+    element();
+}
+function changeSizeSArs(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+    console.log("Normal.");
+    ars.style.height = "100px";
+    ars.style.width = "100px";
+    element();
+}
+//#endregion
+
